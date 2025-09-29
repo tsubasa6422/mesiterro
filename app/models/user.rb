cac261ebe3cb72073c189ea8b9b52579cbc4e3ba
Tsubasa Yamada <220030647+tsubasa6422@users.noreply.github.com>
2025-09-29 03:04:42 +0000

    usersコントローラの追加

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+         has_many :post_images, dependent: :destroy
+
+         has_one_attached :profile_image
+    def get_profile_image
+      unless ptofile_image.attached?
+        file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+        profile_iage.attach(io: File.open(file_path),filename:'default-image.jpg',content_type: 'imaage/jpeg')
+      end
+      profile_image.variant(resize_to_loimit:[width,height]).processed
+    end
+
+
+  end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+
+
+  devise_for :users
+  root to: 'homes#top'
+
+  resources :post_images, only: [:new, :index, :show, :create, :destroy]
+  resources :users, only:[:show :edit]
+
+  get 'homes/about', to: 'homes#about',as: "about"
+end
+

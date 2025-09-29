f8bac6ab5f1e47a6e4f311792e2ac0c66078a674
Tsubasa Yamada <220030647+tsubasa6422@users.noreply.github.com>
2025-09-29 06:54:22 +0000

    コメント機能のためのデータベース作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          has_many :post_images, dependent: :destroy
-
+         has_mnany :post_comments, dependent: :destroy
          has_one_attached :profile_image
     def get_profile_image(width,height)
       unless profile_image.attached?

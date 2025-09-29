bf9ce9fe43a03870a267e520a1ffdbf0bb54cafb
Tsubasa Yamada <220030647+tsubasa6422@users.noreply.github.com>
2025-09-29 08:04:09 +0000

    コメントの投稿、削除機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
 
-  resources :post_images, only: [:new, :index, :show, :create, :destroy]
+  resources :post_images, only: [:new, :index, :show, :create, :destroy] do
+    resources :post_comments, only:[:create,:destroy]
+  end
+  
   resources :users, only:[:show ,:edit,:update]
-
   get 'homes/about', to: 'homes#about',as: "about"
 end
 

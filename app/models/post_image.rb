eb5f702c01b57fe887c8d1463257a35ebdab8ca3
Tsubasa Yamada <220030647+tsubasa6422@users.noreply.github.com>
2025-10-02 05:54:23 +0000

    gem　kaminariの導入、ページネーション機能の追加

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -12,12 +12,13 @@ class PostImage < ApplicationRecord
     favorites.exists?(user_id: user.id)
   end
 
-  def get_image
+  def get_image(width, height)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      image.attach(io: File.open(file_path),filename: 'default-image.jpg',content_type: 'image/jpeg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-      image
+    image.variant(resize_to_fill: [width, height]).processed
   end
+
 end
 
